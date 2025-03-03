f0dc816ef8ccae7287a5304927eb9f7d9a628800
Aionmizu <102879060+Aionmizu@users.noreply.github.com>
2025-03-03 09:32:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Test PHP avec CSS</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+<h1>Bienvenue sur la page de test PHP</h1>
+
+<p><?php echo "Ceci est du texte généré en PHP."; ?></p>
+
+<footer>
+    <p>© <?php echo date('Y'); ?> Mon Projet Test</p>
+</footer>
+
+</body>
+</html>
